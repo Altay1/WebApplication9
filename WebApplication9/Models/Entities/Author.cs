0af0d3fdd35844f50b14fc46f6eed8a4b0b1bc98
Altay Zhenis <Altay7@outlook.com>
2020-06-26 11:59:14 +0600

    Add project files.

---
+++ WebApplication9/Models/Entities/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication9.Models.EnumTypes;
+
+namespace WebApplication9.Models.Entities
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ WebApplication9/Models/EnumTypes/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9.Models.EnumTypes
+{
+    public enum Genre
+    {
+        Detective,
+        Fantasy,
+        Adventure,
+        Mystery,
+        Horor
+    }
+}
